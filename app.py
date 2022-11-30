b0df27f05fba09a3fc2f4385d144bc98612c9a92
MaxRem <mxremez77@gmail.com>
2022-11-30 18:11:45 +0200

    files added

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'Hey there! It is the next work using Heroku ;)'
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+import app as tested_app
+import json
+
+
+class FlaskAppTests(unittest.TestCase):
+	
+	def setUp(self):
+		self.app = tested_app.app.test_client()
+
+	def test_get_hello_endpoint(self):
+		r = self.app.get('/')
+		self.assertEqual(r._status_code, 200)
+		self.assertEqual(r.get_data(), b'Hey there! It is the next work using Heroku ;)')
+
+if __name__ == '__main__':
+	unittest.main()
