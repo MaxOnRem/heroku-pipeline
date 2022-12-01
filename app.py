2c54e1491ed30d1b97cf5da6e3549120f6a0f7c1
MaxRem <72015150+MaxOnRem@users.noreply.github.com>
2022-12-02 00:42:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hey there! It is the next work using Heroku ;)'
+    return 'Hey there!!! It is the next work using Heroku ;)'
 
 if __name__ == '__main__':
     app.run(debug=True)
